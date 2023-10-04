1c682b4417dc302c5ea8c113b4cc733a11cb4a0d
muaadh99 <muaadhmnm.20@uom.lk>
2023-10-04 22:14:03 +0530

    Question 04 QnA added using Python

--- q4/q4.py
+++ q4/q4.py
@@ -1,23 +1,42 @@
 import string
+import time
+
+start = time.time()
+
+
+# Loop through all six-digit numbers
 for i in range (100000,999999,1):
-    x=i//100000
-    y=i%10
     
-    l=i//10000
-    m=l%10
-    k=i%100
-    h=k//10 
+    # Extract first and last digits of the number
+    first_digit=i//100000
+    last_digit=i%10
     
-    a=i//1000
-    b=a%10
-    c=i//100
-    d=c%10
+    # Extract second and fifth digits of the number
+    second_digit=i//10000
+    second_digit=second_digit%10
+    fifth_digit=i%100
+    fifth_digit=fifth_digit//10 
+    
+    # Extract third and fourth digits of the number
+    third_digit=i//1000
+    third_digit=third_digit%10
+    fourth_digit=i//100
+    fourth_digit=fourth_digit%10
        
-    if x==y and m==h and b==d:
-        for h in range (100,999,1):
-            w=i%h
-            s=i//h
-            if w==0 and s<=999:
-                print(i,"::",h,"*",s)
+    # Check if the number is a palindrome
+    if first_digit==last_digit and second_digit==fifth_digit and third_digit==fourth_digit:
+        
+        # Loop through all three-digit numbers
+        for j in range (100,999,1):
+            
+            # Check if the number is divisible by the three-digit number
+            remainder=i%j
+            quotient=i//j
+            if remainder==0 and quotient<=999:
                 
-                
+                # Print the number and the factors
+                print(i,"::",j,"*",quotient)
+
+end = time.time()
+
+print(end - start)
