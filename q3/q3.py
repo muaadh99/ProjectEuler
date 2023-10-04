24fc7f38d050e9fd4ec7b2fcbf0b1780a6ef2f2e
muaadh99 <muaadhmnm.20@uom.lk>
2023-10-04 22:03:47 +0530

    Comments Added To Q3

--- q3/q3.py
+++ q3/q3.py
@@ -1,21 +1,31 @@
 import math
-n=int(input("Input The Number:"))
+
+# Take input from user
+n = int(input("Input The Number:"))
+
+# Print prime factors of the input number
 print("Prime factors")
-i=1
-while(i<=math.sqrt(n)):
-    k=0
-    if (n%i == 0):
-        j=1
-        while(j<=i):
-            if (i%j == 0):
-                k=k+1
-            j=j+1
-        if(k==2):
+
+# Initialize loop variable
+i = 1
+
+# Loop through all numbers less than or equal to the square root of the input number
+while(i <= math.sqrt(n)):
+    k = 0
+
+    # Check if the current number is a factor of the input number
+    if (n % i == 0):
+        j = 1
+
+        # Count the number of factors of the current number
+        while(j <= i):
+            if (i % j == 0):
+                k = k + 1
+            j = j + 1
+
+        # If the current number is prime, print it
+        if(k == 2):
             print(i)
-    i=i+1
-            
 
-
-        
-                                    
-
+    # Increment loop variable
+    i = i + 1
